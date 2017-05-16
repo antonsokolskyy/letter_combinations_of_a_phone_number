d58791ba035815081f44b010e2aec31628b3f7b5
Anton <antonsokolskyy@gmail.com>
2017-05-16 22:34:20 +0300

    small refactor

--- phone_parser.rb
+++ phone_parser.rb
@@ -1,100 +1,92 @@
+# frozen_string_literal: true
+
 require 'pry'
 require 'benchmark'
 
 class PhoneParser
+  attr_reader :matches
   MAX_PHONE_NUMBER_LENGTH = 10
   WORD_MIN_LENGTH = 3
-  LETTERS_MAP = { '2' => ['a', 'b', 'c'],
-                  '3' => ['d', 'e', 'f'],
-                  '4' => ['g', 'h', 'i'],
-                  '5' => ['j', 'k', 'l'],
-                  '6' => ['m', 'n', 'o'],
-                  '7' => ['p', 'q', 'r', 's'],
-                  '8' => ['t', 'u', 'v'],
-                  '9' => ['w', 'x', 'y', 'z']
-                }
+  LETTERS_MAP = { '0' => [],
+                  '1' => [],
+                  '2' => %w[a b c],
+                  '3' => %w[d e f],
+                  '4' => %w[g h i],
+                  '5' => %w[j k l],
+                  '6' => %w[m n o],
+                  '7' => %w[p q r s],
+                  '8' => %w[t u v],
+                  '9' => %w[w x y z] }.freeze
 
-  def initialize
-    @dictionary = {}
-    File.open('./dictionary.txt') do |file|
-      file.each_line do |line|
-        word = line.delete("\n")&.downcase
-        if @dictionary.key?(word.length)
-          @dictionary[word.length] << word
-        else
-          @dictionary[word.length] = []
-        end
-      end
-    end
-    @dictionary.keys.each { |k| @dictionary[k].sort!.uniq! }
+  def initialize(dictionary_path)
+    @matches = []
+    @dictionary = load_dictionary(dictionary_path)
   end
 
   def find_matches(phone)
-    # all matched words
-    matches = []
-    phone = phone.to_s
+    # simple digits-only validation
+    phone = phone.to_i.to_s
+    raise "Phone number should contain #{MAX_PHONE_NUMBER_LENGTH} digits" if phone.length != MAX_PHONE_NUMBER_LENGTH
     # select all possible letters depends on given phone number
     all_letters = phone.chars.map { |digit| LETTERS_MAP[digit] }
-    binding.pry
-    find_next_word(all_letters)
-    # 1.upto(all_letters.size - 1) do |i|
-#       current_possible_letters = all_letters.dup
-#       0.upto(i) do |n|
-#         letters_from_dictionary = @dictionary[i + 1].map { |word| word[n] }.uniq
-#         current_possible_letters[n] = all_letters[n].reject { |letter| !letters_from_dictionary.include?(letter) }
-#       end
-#
-#       words = current_possible_letters[0].product(*current_possible_letters[1..i]).map(&:join)
-#       words.reject! do |word|
-#         @dictionary[i + 1].bsearch { |n| word <=> n }.nil?
-#       end
-#       # binding.pry
-#     end
-    # binding.pry
-    # possible_letters.shift.product(*possible_letters)
+    # find next words recursively
+    find_next_word(all_letters).inspect
+  end
+
+  private
+
+  def load_dictionary(path)
+    raise 'Dictionary path should be a string' unless path.is_a? String
+    d = {}
+    File.open(path) do |file|
+      file.each_line do |line|
+        word = line.delete("\n")&.downcase
+        d.key?(word.length) ? d[word.length] << word : d[word.length] = []
+      end
+    end
+    d.keys.each { |k| d[k].sort!.uniq! }
+    d
   end
 
   def find_next_word(all_letters, start_from = 0, saved_words = [])
-    # binding.pry
+    # recursion exit when last digit reached
     return saved_words if start_from > MAX_PHONE_NUMBER_LENGTH - 1
+
+    # iterate next word from the end of the previous word
     (start_from + 1).upto(all_letters.size - 1) do |i|
+      # min word length is set to 3
       next if i - start_from < WORD_MIN_LENGTH - 1
       current_possible_letters = all_letters.dup
+
+      # remove unnecessary letters(those are not present in dictionary) from all possible letters
       0.upto(i - start_from) do |n|
         letters_from_dictionary = @dictionary[(i + 1) - start_from].map { |word| word[n] }.uniq
-        current_possible_letters[n] = all_letters[n].reject { |letter| !letters_from_dictionary.include?(letter) }
+        current_possible_letters[n] = all_letters[n].select { |letter| letters_from_dictionary.include?(letter) }
       end
+      # create all possible words with given letters
       words = current_possible_letters[start_from].product(*current_possible_letters[(start_from + 1)..i]).map(&:join)
-      # binding.pry
+
       words.each do |word|
+        # skip if the word is not in a dictionary
         next if @dictionary[(i + 1) - start_from].bsearch { |n| word <=> n }.nil?
-        @asdf = find_next_word(all_letters, i + 1, saved_words.dup.push(word))
-        # binding.pry
+        # recursively find next words
+        mathes_data = find_next_word(all_letters, i + 1, saved_words.dup.push(word))
+        # save only the matches with necessary length
+        if mathes_data.join.length == MAX_PHONE_NUMBER_LENGTH
+          @matches << mathes_data
+        end
       end
-      # if i == MAX_PHONE_NUMBER_LENGTH - 1
-      #   # binding.pry
-      #   if saved_words.join.length != MAX_PHONE_NUMBER_LENGTH
-      #     saved_words.shift
-      #   else
-      #     saved_words = [saved_words]
-      #   end
-      # end
     end
-    # binding.pry
-    # if saved_words.join.length != MAX_PHONE_NUMBER_LENGTH
-#       saved_words = []
-#     else
-#       saved_words = [saved_words]
-#     end
-    @asdf
+    @matches
   end
 end
 
-parser = PhoneParser.new
+parser = PhoneParser.new('./full_dictionary.txt')
 
 Benchmark.bm do |x|
   x.report do
-    parser.find_matches(2257222227)
-    # parser.find_matches(6686787825)
+    # parser.find_matches(2257222227)
+    puts parser.find_matches(6_686_787_825)
+    # puts parser.find_matches(66867)
   end
 end
